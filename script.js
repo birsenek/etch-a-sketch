6b2c2b6870d1d0236c499a60f2a4e5d3aa96a492
birsenek <birsenek@gmail.com>
2020-09-20 20:50:45 -0300

    Add grid size options for larger screens

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 const sketch = document.querySelector('.sketch')
-let n
+
+//set grid size
+let gridSize = 16
+
+//create and select Object
+const updateGrid = document.querySelector('select')
+
+//update grid to new selection
+const changeGrid = (e) => {
+    gridSize = document.querySelector('select').value
+    resetGrid()
+    createSquares()
+}
+
+//clear previous grid
+const resetGrid = () => {
+    while (sketch.hasChildNodes()) {
+        sketch.removeChild(sketch.lastChild)
+    }
+}
+
+//listen to changes in select element
+updateGrid.addEventListener('change', changeGrid)
+
 const createSquares = _ => {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
         const square = document.createElement('div')
         square.classList.add('square')
+        if (gridSize == 32) {
+            sketch.style.gridTemplateColumns = 'repeat(32, auto)'
+        } else if (gridSize == 64) {
+            sketch.style.gridTemplateColumns = 'repeat(64, auto)'
+        } else {
+            sketch.style.gridTemplateColumns = 'repeat(16, auto)'
+        }
         sketch.appendChild(square)
     }
 }
@@ -13,6 +43,11 @@ createSquares()
 let isDrawing = false
 sketch.addEventListener('mousedown', e => {
     isDrawing == false ? isDrawing = true : isDrawing = false
+})
+
+//stop drawing when changing grid
+updateGrid.addEventListener('change', e => {
+    isDrawing = false 
 })
 
 sketch.addEventListener('touchstart', e => {
@@ -29,7 +64,6 @@ sketch.addEventListener('touchmove', e => {
     isDrawing = true
     let touch = e.touches[0];
     const square = document.elementFromPoint(touch.clientX, touch.clientY)
-    console.log(touch.clientY)
     if(touch.clientY > 215 && touch.clientY < 550 && touch.clientX > 6 && touch.clientX < 350) {
         const squareColor = document.getElementById('squareColor').value
         square.style.background = squareColor
@@ -52,3 +86,4 @@ clearScreenButton.addEventListener('click', e => {
         isDrawing = false
     })
 })
+
